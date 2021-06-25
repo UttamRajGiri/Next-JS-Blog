1993692a57db652f855c62ac1e63ceebce9dac88
Rumsan Associates <depenti.karki@rumsan.com>
2021-06-25 14:03:39 +0545

    fixes

--- components/navbar.js
+++ components/navbar.js
@@ -25,15 +25,17 @@ export default function Navbar() {
                         </div>
                         <div className="hidden sm:block sm:ml-6">
                             <div className="flex space-x-4">
+
                                 <Link href="/">
                                     <a className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
                                 </Link>
-
+                                <Link href="/about">
+                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                                </Link>
                                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
 
                                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
                             </div>
                         </div>
                     </div>
